dd24a21c73e8fe7010f2c4afb84df49b2772a0a5
RaymondwChen <70980012+RaymondwChen2@users.noreply.github.com>
2021-11-30 18:11:26 -0800

    leetcode

--- reverse_string_vowel.js
+++ reverse_string_vowel.js
@@ -1,3 +1,31 @@
+// 345. Reverse Vowels of a String
+// Easy
+
+// 1322
+
+// 1670
+
+// Add to List
+
+// Share
+// Given a string s, reverse only all the vowels in the string and return it.
+
+// The vowels are 'a', 'e', 'i', 'o', and 'u', and they can appear in both cases.
+
+
+
+//     Example 1:
+
+// Input: s = "hello"
+// Output: "holle"
+// Example 2:
+
+// Input: s = "leetcode"
+// Output: "leotcede"
+
+
+
+
 var reverseVowels = function (s) {
     let vowels = 'aeiou';
     let queue = [];
